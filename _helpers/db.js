69b7b4e7722d5b6c7c906f9bad988bc324c594c2
SHAHARYAR1255 <shaharyar.malik2000@gmail.com>
2023-01-12 16:11:06 +0500

    onn

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,6 +2,8 @@ const config = require('config.json');
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+console.log(process.env.MONGODB_URI, 'asdjasd');
+
 mongoose.Promise = global.Promise;
 
 module.exports = {
